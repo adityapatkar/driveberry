6ec8295b481148825d47fd980de0195b5eee33f0
Aditya Patkar <aditya.patkar@nonstopio.com>
2023-11-28 11:44:38 -0500

    Increased speed

--- main.py
+++ main.py
@@ -108,13 +108,14 @@ class DriveBerry(object):
                 logging.debug(f"Processing frame {i}")
                 self.video_orig.write(lane_frame)
 
-                object_frame = self.process_objects_on_road(object_frame)
-                self.video_objs.write(object_frame)
                 # show_image("Detected Objects", object_frame)
 
                 lane_frame = self.lane_follower.follow_lane(lane_frame)
                 self.video_lane.write(lane_frame)
                 # cv2.imshow("Lane Lines", lane_frame)
+
+                object_frame = self.process_objects_on_road(object_frame)
+                self.video_objs.write(object_frame)
 
                 if cv2.waitKey(1) & 0xFF == ord("q"):
                     break
